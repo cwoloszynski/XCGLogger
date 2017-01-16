2d49865fcd5a7619f132b8c5cd29e0b43bfda0d4
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2017-01-16 16:32:03 +0000

    Add linux testing

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 let package = Package(
 	name: "XCGLogger",
 	targets: [
-		Target(name: "XCGLogger")
-	]
+		Target(name: "XCGLogger"),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/vapor/polymorphic.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
+    ]
 )
